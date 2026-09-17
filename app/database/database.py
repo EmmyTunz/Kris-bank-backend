baf833f8132c3df11f5a7a35a9554368dffa679a
EmmyTunz <tummagold@gmail.com>
2026-09-17 11:05:38 +0100

    setup database and alembic migrations

---
+++ app/database/database.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+with engine.connect() as connection:
+    print("Database connected successfully!")
--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="kris Bank Api")
+
+@app.get("/")
+def root():
+    return {"Message": "Kris Bank API"}
 
